bc6dbe119df94d59a306c93bd9f01ad42e7a31ed
Ziqi Zhang <ziqizhang@lawn-128-61-36-53.lawn.gatech.edu>
2021-09-14 23:44:29 -0400

    update admm

--- src/g_glad.py
+++ src/g_glad.py
@@ -6,7 +6,6 @@ import torch.nn as nn
 from torch.optim import Adam
 import numpy as np
 from torch_sqrtm import MatrixSquareRoot
-from g_ista import *
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -36,7 +35,7 @@ class G_glad():
         else:
             self.empir_cov = pre_cov.to(device)
         
-        self.diag_mask = torch.eye(self.ngenes).to(device)
+        # self.diag_mask = torch.eye(self.ngenes).to(device)
 
         # shape (ntimes, ntimes)
         # self.kernel = ((np.ones(self.ntimes)/self.ntimes)*K)/np.sum(K,axis=0)
@@ -75,38 +74,37 @@ class G_glad():
     def train(self, t, max_iters = 500, n_intervals = 10, lamb = 2.1e-4, rho = 1, beta = 0, theta_init_offset = 0.1):
         # k of the shape: (ntimes, 1, 1)
         w = self.weights[t,:][:,None, None]
-
+        # check to make sure w sum up to 1
         assert (w.sum() - 1).abs() < 1e-6
 
         empir_cov_ave = torch.sum(w * self.empir_cov, dim = 0)
         # initialize
         Z = torch.diag_embed(1/(torch.diagonal(empir_cov_ave, offset=0, dim1=-2, dim2=-1) + theta_init_offset))
+        # make symm + PD
         ll = torch.cholesky(Z)
         Z = torch.matmul(ll, ll.transpose(-1, -2))
-        self.theta = Z
 
         I = torch.eye(Z.shape[0]).to(device)
         zero = torch.Tensor([0]).to(device)
 
         it = 0
-        loss1 = self.neg_lkl_loss(self.theta, empir_cov_ave)
-        loss2 = (self.theta - self.diag_mask * self.theta).abs().sum()
-        loss3 = torch.norm(self.mask * self.theta)
+        loss1 = self.neg_lkl_loss(Z, empir_cov_ave)
+        loss2 = Z.abs().sum()
+        loss3 = torch.norm(self.mask * Z)
         print("Initial: loss1: {:.5f}, loss2: {:.5f}, loss3: {:.5f}".format(loss1.item(), loss2.item(), loss3.item()))
  
         while(it < max_iters):  
-            # Y of the shape: (ngenes, ngenes)
             Y = empir_cov_ave/rho - Z            
-            self.theta = - 0.5 * Y + torch_sqrtm(Y.t() @ Y * 0.25 + I/rho + 1e-6)
-            # Z = torch.sign(self.theta) * torch.max((self.theta).abs() - lamb/rho, zero)
-            Z = torch.sign(self.theta) * torch.max((rho * (self.theta).abs() - lamb)/(rho + beta * self.mask), zero)
-            assert self.theta.shape == (self.ngenes, self.ngenes)
+            theta = - 0.5 * Y + torch_sqrtm(Y.t() @ Y * 0.25 + I/rho + 1e-6)
+            Z_pre = Z.detach().clone()
+            Z = torch.sign(theta) * torch.max((rho * theta.abs() - lamb)/(rho + beta * self.mask), zero)
+            assert Z.shape == (self.ngenes, self.ngenes)
 
             # Validation
             if (it + 1) % n_intervals == 0:
-                loss1 = self.neg_lkl_loss(self.theta, empir_cov_ave)
-                loss2 = (self.theta - self.diag_mask * self.theta).abs().sum()
-                loss3 = torch.norm(self.mask * self.theta)
+                loss1 = self.neg_lkl_loss(Z, empir_cov_ave)
+                loss2 = Z.abs().sum()
+                loss3 = torch.norm(self.mask * Z)
                 valid_loss = loss1 + lamb * loss2 + beta * loss3
                 print("n_iter: {}, loss: {:.8f}".format(it+1, valid_loss.item()))
                 
@@ -117,15 +115,22 @@ class G_glad():
                 ]
                 for i in info:
                     print("\t", i)
+            
+            # stopping criteria
+            residual = rho * (Z_pre - Z).pow(2).sum().sqrt()
+            # free up memory
+            del Z_pre
+            if residual < 1e-6:
+                break
             it += 1
 
-        return (self.theta * (1 - self.diag_mask)).data.cpu().numpy()
-        # return self.theta.data.cpu().numpy()
+        # return sparse version
+        return Z.cpu().numpy()
 
 
 # GLAD tensor: data shape (ntimes, ngenes, ngenes)
 class G_glad_batch():
-    def __init__(self, X, K, TF = None, seed = 0, theta_init = None, pre_cov = None):
+    def __init__(self, X, K, TF = None, seed = 0, pre_cov = None):
         super(G_glad_batch, self).__init__()
         # set random seed
         if seed is not None:
@@ -135,7 +140,6 @@ class G_glad_batch():
         self.X = torch.FloatTensor(X).to(device)
         self.ntimes, self.nsamples, self.ngenes = self.X.shape
 
-        # TODO: consider the mean for variance calculation
         if pre_cov is None:
             # shape (ntimes, nsamples, ngenes)
             self.epir_mean = self.X.mean(dim = 1, keepdim = True).expand(self.ntimes, self.nsamples, self.ngenes)
@@ -166,7 +170,7 @@ class G_glad_batch():
             # element-wise reverse
             self.mask = 1 - self.mask
             self.mask = self.mask.expand(self.ntimes, self.ngenes, self.ngenes)   
-        
+
     @staticmethod
     def neg_lkl_loss(thetas, S):
         """\
@@ -193,33 +197,31 @@ class G_glad_batch():
 
         # initialize
         Z = torch.diag_embed(1/(torch.diagonal(self.w_empir_cov, offset=0, dim1=-2, dim2=-1) + theta_init_offset))
-        # positive definite matrix
+        # make symm + PD
         ll = torch.cholesky(Z)
         Z = torch.matmul(ll, ll.transpose(-1, -2))
-        self.thetas = Z
-        
         I = torch.eye(self.ngenes).expand(Z.shape).to(device)
 
         it = 0
-        loss1 = self.neg_lkl_loss(self.thetas, self.w_empir_cov).sum()
-        loss2 = (self.thetas - self.diag_mask * self.thetas).abs().sum()
-        loss3 = torch.norm(self.mask * self.thetas)
+        loss1 = self.neg_lkl_loss(Z, self.w_empir_cov).sum()
+        loss2 = Z.abs().sum()
+        loss3 = torch.norm(self.mask * Z)
         print("Initial: loss1: {:.5f}, loss2: {:.5f}, loss3: {:.5f}".format(loss1.item(), loss2.item(), loss3.item()))
 
         while(it < max_iters):   
             # Y of the shape: (ntimes, ngenes, ngenes)
             Y = self.w_empir_cov/rho - Z
 
-            self.thetas = - 0.5 * Y + torch.stack([torch_sqrtm(mat) for mat in (torch.transpose(Y,1,2) @ Y * 0.25 + I/rho + 1e-6)])
-            # Z = torch.sign(self.thetas) * torch.max(((self.thetas).abs() - lamb/rho), torch.Tensor([0]).to(device))
-            Z = torch.sign(self.thetas) * torch.max((rho * (self.thetas).abs() - lamb)/(rho + beta * self.mask), torch.Tensor([0]).to(device))
-            assert self.thetas.shape == (self.ntimes, self.ngenes, self.ngenes)
+            thetas = - 0.5 * Y + torch.stack([torch_sqrtm(mat) for mat in (torch.transpose(Y,1,2) @ Y * 0.25 + I/rho + 1e-6)])
+            Z = torch.sign(thetas) * torch.max((rho * thetas.abs() - lamb)/(rho + beta * self.mask), torch.Tensor([0]).to(device))
+            Z_pre = Z.detach().clone()
+            assert Z.shape == (self.ntimes, self.ngenes, self.ngenes)
 
             # Validation
             if (it + 1) % n_intervals == 0:
-                loss1 = self.neg_lkl_loss(self.thetas, self.w_empir_cov).sum()
-                loss2 = (self.thetas - self.diag_mask * self.thetas).abs().sum()
-                loss3 = torch.norm(self.mask * self.thetas)
+                loss1 = self.neg_lkl_loss(Z, self.w_empir_cov).sum()
+                loss2 = Z.abs().sum()
+                loss3 = torch.norm(self.mask * Z)
                 valid_loss = loss1 + lamb * loss2 + beta * loss3
                 print("n_iter: {}, loss: {:.8f}".format(it+1, valid_loss.item()))
                 
@@ -230,12 +232,22 @@ class G_glad_batch():
                 ]
                 for i in info:
                     print("\t", i)
+
+            # stopping criteria
+            residual = rho * (Z_pre - Z).pow(2).sum().sqrt()
+            # free up memory
+            del Z_pre
+            if residual < 1e-6:
+                break
+            
             it += 1
             
-        return (self.thetas * (1 - self.diag_mask)).data.cpu().numpy()
-        # return self.theta.data.cpu().numpy()
+        # return sparse version
+        return Z.cpu().numpy()
 
 
+
+'''
 # vanilla gradient descent
 class G_glad_grad():
     def __init__(self, X, K, lr = 1e-3, seed = 0, theta_init = None):
@@ -335,4 +347,4 @@ class G_glad_grad():
                         if self.optimizer.param_groups[0]['lr'] < 1e-6:
                             break
         return self.theta
-
+'''
