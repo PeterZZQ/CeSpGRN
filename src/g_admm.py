21d787452f9724128779a6c548ad04cba911bd64
Ziqi Zhang <ziqizhang@lawn-128-61-36-53.lawn.gatech.edu>
2021-09-14 23:44:29 -0400

    update admm

--- src/g_admm.py
+++ src/g_admm.py
@@ -12,7 +12,7 @@ torch_sqrtm = MatrixSquareRoot.apply
 
 # ADMM
 class G_admm():
-    def __init__(self, X, K, TF = None, seed = 0):
+    def __init__(self, X, K, TF = None, seed = 0, pre_cov = None):
         super(G_admm, self).__init__()
         # set random seed
         if seed is not None:
@@ -21,18 +21,18 @@ class G_admm():
         # shape (ntimes, nsamples, ngenes)
         self.X = torch.FloatTensor(X).to(device)
         self.ntimes, self.nsamples, self.ngenes = self.X.shape
-
-        # TODO: consider the mean for variance calculation
-        # shape (ntimes, nsamples, ngenes)
-        self.epir_mean = self.X.mean(dim = 1, keepdim = True).expand(self.ntimes, self.nsamples, self.ngenes)
-        X = self.X - self.epir_mean
-        # (ntimes * nsamples, ngenes, ngenes)
-        self.empir_cov = torch.bmm(X.reshape((self.ntimes * self.nsamples, self.ngenes, 1)), X.reshape((self.ntimes * self.nsamples, 1, self.ngenes)))
-
-        # (ntimes, ngenes, ngenes)
-        self.empir_cov = torch.sum(self.empir_cov.reshape((self.ntimes, self.nsamples, self.ngenes, self.ngenes)), dim = 1)/(self.nsamples - 1)
         
-        self.diag_mask = torch.eye(self.ngenes).to(device)
+        if pre_cov is None:
+            # shape (ntimes, nsamples, ngenes)
+            self.epir_mean = self.X.mean(dim = 1, keepdim = True).expand(self.ntimes, self.nsamples, self.ngenes)
+            X = self.X - self.epir_mean
+            # (ntimes * nsamples, ngenes, ngenes)
+            self.empir_cov = torch.bmm(X.reshape((self.ntimes * self.nsamples, self.ngenes, 1)), X.reshape((self.ntimes * self.nsamples, 1, self.ngenes)))
+            # (ntimes, ngenes, ngenes)
+            self.empir_cov = torch.sum(self.empir_cov.reshape((self.ntimes, self.nsamples, self.ngenes, self.ngenes)), dim = 1)/(self.nsamples - 1)
+
+        else:
+            self.empir_cov = pre_cov
 
         # shape (ntimes, ntimes)
         # self.kernel = ((np.ones(self.ntimes)/self.ntimes)*K)/np.sum(K,axis=0)
@@ -70,17 +70,11 @@ class G_admm():
         return t1 + t2
 
 
-    def train(self, t, max_iters = 50, n_intervals = 1, lamb = 2.1e-4, rho = 1, beta = 0, theta_init_offset = 0.1):
+    def train(self, t, max_iters = 50, n_intervals = 1, lamb = 2.1e-4, rho = None, beta = 0, theta_init_offset = 0.1):
         # kernel function
         w = self.weights[t,:][:,None, None]
-        
-        """
-        import matplotlib.pyplot as plt
-        plt.plot(w.cpu().numpy().squeeze())
-        plt.show()
-        """
-
         assert (w.sum() - 1).abs() < 1e-6
+
         # weight average of empirical covariance matrix, [ngenes, ngenes]
         empir_cov_ave = torch.sum(w * self.empir_cov, dim = 0)
         # initialize
@@ -88,44 +82,71 @@ class G_admm():
         # positive definite matrix
         ll = torch.cholesky(Z)
         Z = torch.matmul(ll, ll.transpose(-1, -2))
-        self.theta = Z
         
         U = torch.zeros(Z.shape).to(device)
         I = torch.eye(Z.shape[0]).to(device)
         zero = torch.Tensor([0]).to(device)
 
         it = 0
-        loss1 = self.neg_lkl_loss(self.theta, empir_cov_ave)
-        loss2 = (self.theta - self.diag_mask * self.theta).abs().sum()
-        loss3 = torch.norm(self.mask * self.theta)
-        print("Initial: loss1: {:.5f}, loss2: {:.5f}, loss3: {:.5f}".format(loss1.item(), loss2.item(), loss3.item()))
+        if rho is None:
+            updating_rho = True
+            rho = 1
+        else:
+            updating_rho = False
+          
+        while(it < max_iters): 
+            # Primal
+            Y = U - Z + empir_cov_ave/rho # (ngenes, ngenes)
+            theta = - 0.5 * Y + torch_sqrtm(Y.t() @ Y * 0.25 + I/rho)
+            Z_pre = Z.detach().clone()
+            Z = torch.sign(theta + U) * torch.max((rho * (theta + U).abs() - lamb)/(rho + beta * self.mask), zero)
+            assert Z.shape == (self.ngenes, self.ngenes)
 
+            # Dual
+            U = U + theta - Z
 
-        while(it < max_iters):   
-            # Y of the shape: (ngenes, ngenes)
-            Y = U - Z + empir_cov_ave/rho
-            self.theta = - 0.5 * Y + torch_sqrtm(Y.t() @ Y * 0.25 + I/rho)
-            Z = torch.sign(self.theta + U) * torch.max((rho * (self.theta + U).abs() - lamb)/(rho + beta * self.mask), zero)
-            U = U + self.theta - Z
-            assert self.theta.shape == (self.ngenes, self.ngenes)
-
-            # Validation
+            # updating rho
+            if updating_rho:
+                # calculate residual to adjust for rho, can also be used as stopping criteria
+                primal_residual = torch.sqrt((theta - Z).pow(2).sum())
+                dual_residual = rho * torch.sqrt((Z - Z_pre).pow(2).sum())
+                if primal_residual > 10 * dual_residual:
+                    rho =  rho * 2
+                elif dual_residual > 10 * primal_residual:
+                    rho = rho / 2
+            # free-up memory
+            del Z_pre
+            # Stopping criteria
             if (it + 1) % n_intervals == 0:
-                loss1 = self.neg_lkl_loss(self.theta, empir_cov_ave)
-                loss2 = (self.theta - self.diag_mask * self.theta).abs().sum()
-                loss3 = torch.norm(self.mask * self.theta)
-                valid_loss = loss1 + lamb * loss2 + beta * loss3
-                print("n_iter: {}, loss: {:.8f}".format(it+1, valid_loss.item()))
-                
-                info = [
-                    'negative log lkl: {:.5f}'.format(loss1.item()),
-                    'sparsity loss: {:.5f}'.format(loss2.item()),
-                    'TF knowledge loss: {:.5f}'.format(loss3.item())
-                ]
-                for i in info:
-                    print("\t", i)
+                # calculate duality gap
+                # loss = self.neg_lkl_loss(theta, empir_cov_ave) + lamb * Z.abs().sum() + beta * (self.mask * Z).pow(2).sum()
+                # primal_val = loss  + rho/2 * (theta - Z).pow(2).sum()
+                # dual_val = loss + rho/2 * (theta - Z + U).pow(2).sum() - rho/2 * U.pow(2).sum()
+                # duality_gap = primal_val - dual_val
+
+                # can be simplified
+                duality_gap = rho * torch.trace(U.T @ (Z - theta))
+                print("n_iter: {}, duality gap: {:.4e}, primal residual: {:.4e}, dual residual: {:4e}".format(it+1, duality_gap.item(), primal_residual.item(), dual_residual.item()))
+                # duality gap is reducing extremely fast, and even duality gap reduce sometime, dual residual may explod
+                if duality_gap.abs() < 1e-12:
+                    break
+
+                # epsilon_abs = 1e-4
+                # epsilon_rel = 1e-4
+                # primal_eps = np.sqrt(theta.shape[0]) * epsilon_abs + epsilon_rel * max(theta.pow(2).sum().sqrt(), Z.pow(2).sum().sqrt())
+                # dual_eps = np.sqrt(theta.shape[0]) * epsilon_abs + epsilon_rel * U.pow(2).sum().sqrt()
+                # print("primal_eps: {:.4e}, dual_eps: {:.4e}".format(primal_eps, dual_eps))
+                # if (primal_residual < primal_eps) and (dual_residual < dual_eps):
+                #     break
+
             it += 1
-        return (self.theta * (1 - self.diag_mask)).data.cpu().numpy()
+        
+        loss1 = self.neg_lkl_loss(Z, empir_cov_ave)
+        loss2 = Z.abs().sum()
+        loss3 = (self.mask * Z).pow(2).sum()
+        print("Final: loss1: {:.5f}, loss2: {:.5f}, loss3: {:.5f}".format(loss1.item(), loss2.item(), loss3.item()))
+
+        return Z.cpu().numpy()
 
 
 # ADMM tensor: data shape (ntimes, ngenes, ngenes)
@@ -140,7 +161,6 @@ class G_admm_batch():
         self.X = torch.FloatTensor(X).to(device)
         self.ntimes, self.nsamples, self.ngenes = self.X.shape
 
-        # TODO: consider the mean for variance calculation
         if pre_cov is None:
             # shape (ntimes, nsamples, ngenes)
             self.epir_mean = self.X.mean(dim = 1, keepdim = True).expand(self.ntimes, self.nsamples, self.ngenes)
@@ -151,8 +171,6 @@ class G_admm_batch():
             self.empir_cov = torch.sum(self.empir_cov.reshape((self.ntimes, self.nsamples, self.ngenes, self.ngenes)), dim = 1)/(self.nsamples - 1)
         else:
             self.empir_cov = pre_cov
-
-        self.diag_mask = torch.eye(self.ngenes).expand(self.empir_cov.shape).to(device)
 
         # shape (ntimes, ntimes)
         # self.kernel = ((np.ones(self.ntimes)/self.ntimes)*K)/np.sum(K,axis=0)
@@ -201,6 +219,7 @@ class G_admm_batch():
         --------------
             The negative log likelihood value
         """
+        # logdet works for batches of matrices, give a high dimensional data
         t1 = -1*torch.logdet(thetas)
         t2 = torch.stack([torch.trace(mat) for mat in torch.bmm(S, thetas)])
         return t1 + t2
@@ -211,44 +230,70 @@ class G_admm_batch():
        # initialize
         Z = torch.diag_embed(1/(torch.diagonal(self.w_empir_cov, offset=0, dim1=-2, dim2=-1) + theta_init_offset))
         # positive definite matrix
-        ll = torch.linalg.cholesky(Z)
+        ll = torch.cholesky(Z)
         Z = torch.matmul(ll, ll.transpose(-1, -2))
-        self.thetas = Z
         
         U = torch.zeros(Z.shape).to(device)
         I = torch.eye(self.ngenes).expand(Z.shape).to(device)
 
         it = 0
-        loss1 = self.neg_lkl_loss(self.thetas, self.w_empir_cov).sum()
-        loss2 = (self.thetas - self.diag_mask * self.thetas).abs().sum()
-        loss3 = torch.norm(self.mask * self.thetas)
-        print("Initial: loss1: {:.5f}, loss2: {:.5f}, loss3: {:.5f}".format(loss1.item(), loss2.item(), loss3.item()))
+        if rho is None:
+            updating_rho = True
+            # rho of the shape (ntimes, 1, 1)
+            rho = torch.ones((Z.shape[0], 1, 1))
+        else:
+            updating_rho = False
 
-        while(it < max_iters):   
-            # Y of the shape: (ntimes, ngenes, ngenes)
-            Y = U - Z + self.w_empir_cov/rho
+        while(it < max_iters): 
+            # Primal 
+            Y = U - Z + self.w_empir_cov/rho    # (ntimes, ngenes, ngenes)
+            thetas = - 0.5 * Y + torch.stack([torch_sqrtm(mat) for mat in (torch.transpose(Y,1,2) @ Y * 0.25 + I/rho)])
+            Z_pre = Z.detach().clone()            
+            Z = torch.sign(thetas + U) * torch.max((rho * (thetas + U).abs() - lamb)/(rho + beta * self.mask), torch.Tensor([0]).to(device))
+            assert Z.shape == (self.ntimes, self.ngenes, self.ngenes)
 
-            self.thetas = - 0.5 * Y + torch.stack([torch_sqrtm(mat) for mat in (torch.transpose(Y,1,2) @ Y * 0.25 + I/rho)])
-            Z = torch.sign(self.thetas + U) * torch.max((rho * (self.thetas + U).abs() - lamb)/(rho + beta * self.mask), torch.Tensor([0]).to(device))
-            U = U + self.thetas - Z
+            # Dual
+            U = U + thetas - Z
 
-            assert self.thetas.shape == (self.ntimes, self.ngenes, self.ngenes)
+            # updating rho, rho should be of shape (ntimes, 1, 1)
+            if updating_rho:
+                # calculate residual to adjust for rho, can also be used as stopping criteria
+                # primal_residual and dual_residual of the shape (ntimes, 1, 1)
+                primal_residual = torch.sqrt((thetas - Z).pow(2).sum(1).sum(1))
+                # print(Z.shape)
+                # print(Z_pre.shape)
+                # print(rho.shape)
+                dual_residual = rho.squeeze() * torch.sqrt((Z - Z_pre).pow(2).sum(1).sum(1))
+                # print(primal_residual.shape)
+                # print(dual_residual.shape)
+                mask_inc = (primal_residual > 10 * dual_residual)
+                rho[mask_inc, :, :] = rho[mask_inc, :, :] * 2
+                mask_dec = (dual_residual > 10 * primal_residual)
+                rho[mask_dec, :, :] = rho[mask_dec, :, :] / 2
 
-            # Validation
-            if (it + 1) % n_intervals == 0:
-                loss1 = self.neg_lkl_loss(self.thetas, self.w_empir_cov).sum()
-                loss2 = (self.thetas - self.diag_mask * self.thetas).abs().sum()
-                loss3 = torch.norm(self.mask * self.thetas)
-                valid_loss = loss1 + lamb * loss2 + beta * loss3
-                print("n_iter: {}, loss: {:.8f}".format(it+1, valid_loss.item()))
-                
-                info = [
-                    'negative log lkl: {:.5f}'.format(loss1.item()),
-                    'sparsity loss: {:.5f}'.format(loss2.item()),
-                    'TF knowledge loss: {:.5f}'.format(loss3.item())
-                ]
-                for i in info:
-                    print("\t", i)
-            it += 1
+            # free-up memory
+            del Z_pre
             
-        return (self.thetas * (1 - self.diag_mask)).data.cpu().numpy()
+            # Stopping criteria
+            if (it + 1) % n_intervals == 0:
+                # calculate sum of all duality gap
+                # loss = self.neg_lkl_loss(thetas, self.w_empir_cov).sum() + lamb * Z.abs().sum() + beta * (self.mask * Z).pow(2).sum()
+                # primal_val = loss  + rho/2 * (thetas - Z).pow(2).sum()
+                # dual_val = loss + rho/2 * (thetas - Z + U).pow(2).sum() - rho/2 * U.pow(2).sum()
+                # duality_gap = primal_val - dual_val
+
+                # simplify sum of all duality gap
+                duality_gap = rho * torch.stack([torch.trace(mat) for mat in torch.bmm(U.permute(0,2,1), Z - thetas)])
+                duality_gap = duality_gap.sum()
+                print("n_iter: {}, duality gap: {:.8f}".format(it+1, duality_gap.item()))
+                
+                if duality_gap < 1e-9:
+                    break
+            it += 1
+
+        loss1 = self.neg_lkl_loss(Z, self.w_empir_cov).sum()
+        loss2 = Z.abs().sum()
+        loss3 = (self.mask * Z).pow(2).sum()
+        print("Final: loss1: {:.5f}, loss2: {:.5f}, loss3: {:.5f}".format(loss1.item(), loss2.item(), loss3.item()))   
+
+        return Z.cpu().numpy()
