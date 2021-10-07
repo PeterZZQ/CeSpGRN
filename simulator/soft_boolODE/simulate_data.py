edff53423597c157c6c5a0a8a817301f2564125e
PeterZZQ <zhangziqibuaa@gmail.com>
2021-10-07 16:50:39 -0400

    update the simulator to keep the in/out degree of the graph

--- simulator/soft_boolODE/simulate_data.py
+++ simulator/soft_boolODE/simulate_data.py
@@ -7,9 +7,8 @@ from umap import UMAP
 from sklearn.decomposition import PCA
 import numpy as np
 
-# In[] bifurcating
-import importlib
-importlib.reload(simulator)
+plt.rcParams["font.size"] = 20
+
 def preprocess(counts):
     """\
     Input:
@@ -26,9 +25,13 @@ def preprocess(counts):
     counts = np.log1p(counts)
     return counts
 
-plt.rcParams["font.size"] = 20
+import importlib
+importlib.reload(simulator)
+
+# In[] bifurcating
 # the larger the stepsize, the noisier the trajectory is
 # the smaller the m_gene is, the noisier the trajectory is
+'''
 stepsize = 0.0001
 simu_setting = {"ncells": 1000, # number of cells
                 "ntimes": 1000, # time length for euler simulation
@@ -72,8 +75,60 @@ fig.savefig("bifur/observed_count_plot.png", bbox_inches = "tight")
 np.save("bifur/true_count.npy", results["true count"].T)
 np.save("bifur/obs_count.npy", results["observed count"].T)
 np.save("bifur/pseudotime.npy", results["pseudotime"])
+'''
+# In[1] simulate bifurcating data where the graph in/out degrees kept
+path = "../data/continuousODE/bifur_sample_keep_deg/"
+stepsize = 0.0001
+simu_setting = {"ncells": 1000, # number of cells
+                "ntimes": 1000, # time length for euler simulation
+                "integration_step_size": stepsize, # stepsize for each euler step
+                # parameter for dyn_GRN
+                "ngenes": 18, # number of genes 
+                "mode": "TF-TF&target", # mode of the simulation, `TF-TF&target' or `TF-target'
+                "ntfs": 5,  # number of TFs
+                # nchanges also drive the trajectory, but even if don't change nchanges, there is still linear trajectory
+                "nchanges": 5, # number of changing edges for each interval
+                "change_stepsize": 100, # number of stepsizes for each change
+                "density": 0.1, # number of edges
+                "seed": 0, # random seed
+                "dW": None,
+                "backbone": np.array(["0_1"] * 300 + ["1_2"] * 350 + ["1_3"] * 350),
+                "keep_degree": True,
+                "G0": None
+                }
+results = simulator.run_simulator(**simu_setting)
 
+fig = plt.figure(figsize = (10, 7))
+ax = fig.add_subplot()
+umap_op = UMAP(n_components = 2)
+pca_op = PCA(n_components = 2)
 
-# %%
+X = results["true count"].T
+pt = results["pseudotime"]
+GRNs = results["GRNs"]
+# make the graph symmetric, originally only one direction, so we can simply sum them up
+
+# don't calculate diagonal elment twice
+diag_values = np.concatenate([np.diag(np.diag(GRNs[x, :, :]))[None, :, :] for x in range(GRNs.shape[0])], axis = 0)
+GRNs = GRNs + np.transpose(GRNs, (0, 2, 1)) - diag_values
+
+X = preprocess(X)
+X_umap = pca_op.fit_transform(X)
+ax.scatter(X_umap[:, 0], X_umap[:, 1], s = 5, c = pt)
+fig.savefig(path + "true_count_plot.png", bbox_inches = "tight")
+
+fig = plt.figure(figsize = (10, 7))
+ax = fig.add_subplot()
+X = results["observed count"].T
+pt = results["pseudotime"]
+X = preprocess(X)
+X_umap = pca_op.fit_transform(X)
+ax.scatter(X_umap[:, 0], X_umap[:, 1], s = 5, c = pt)
+fig.savefig(path + "observed_count_plot.png", bbox_inches = "tight")
+
+np.save(path + "true_count.npy", results["true count"].T)
+np.save(path + "obs_count.npy", results["observed count"].T)
+np.save(path + "pseudotime.npy", results["pseudotime"])
+np.save(path + "GRNs.npy", GRNs)
 
 # %%
