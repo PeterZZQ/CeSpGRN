43c8ee058849a0c17affe89d5f60e9d28017c2ad
Ziqi Zhang <ziqi@br011.ib.bridges2.psc.edu>
2021-11-09 20:56:58 -0500

    include de analysis

--- data/COUNTS-THP-1/script.py
+++ data/COUNTS-THP-1/script.py
@@ -11,9 +11,12 @@ anno.index = counts.index.values
 anno.to_csv("anno.csv")
 genes = pd.DataFrame(data = counts.columns.values[:,None])
 genes.to_csv("genes.csv", index = False, header = False)
+# use raw count or log-transformed for scode?
 counts.T.to_csv("expr.txt", header = False, index = False, sep = "\t")
 
+# the distribution of count is log-normal
 adata = AnnData(X = counts.values)
+sc.pp.log1p(adata)
 adata.uns['iroot'] = 0
 sc.pp.neighbors(adata, n_neighbors = 30)
 sc.tl.diffmap(adata, n_comps = 5)
@@ -25,69 +28,4 @@ pt_est.to_csv("dpt_time.txt", sep = "\t", index = True, header = False)
 sc.pl.diffmap(adata, color = "dpt_pseudotime")
 
 
-# In[] GRN 
-gt = pd.read_excel("gt_matrix.xlsx")
-
-grn_pos = {}
-grn_neg = {}
-for idx, row in gt.iterrows():
-    if row["direction"] == "activate":
-        if not pd.isna(row["Input gene"]):
-            tf = row["Input gene"]
-            if tf not in grn_pos.keys():
-                grn_pos[tf] = []
-
-        grn_pos[tf].extend([x for x in row[2:].values if not pd.isna(x)])
-
-    elif row["direction"] == "suppress":
-        if not pd.isna(row["Input gene"]):
-            tf = row["Input gene"]
-            if tf not in grn_neg.keys():
-                grn_neg[tf] = []
-
-        grn_neg[tf].extend([x for x in row[2:].values if not pd.isna(x)])       
-    else:
-        raise ValueError("either activate or suppress")
-
-# In[]
-tfs = sorted(list(set([x for x in grn_pos.keys()]).union([x for x in grn_neg.keys()])))
-targets = []
-for tf in grn_pos.keys():
-    genes = grn_pos[tf]
-    for gene in genes:
-        if gene not in tfs:
-            targets.append(gene)
-
-for tf in grn_neg.keys():
-    genes = grn_neg[tf]
-    for gene in genes:
-        if gene not in tfs:
-            targets.append(gene)
-
-targets = sorted(list(set(targets)))
-
-grn = pd.DataFrame(index = tfs + targets, columns = tfs + targets, data = 0)
-for tf in grn_pos.keys():
-    genes = grn_pos[tf]
-    grn.loc[tf, genes] += 1
-print(np.max(grn.values))
-for tf in grn_neg.keys():
-    genes = grn_neg[tf]
-    assert np.sum(grn.loc[tf, genes].values) == 0
-    grn.loc[tf, genes] -= 1
-print(np.min(grn.values))
-
-
-# In[]
-
-genes = [x for x in pd.read_csv("genes.csv", header = None).values.squeeze()]
-overlap_genes = set(genes).intersection(set(tfs + targets))
-
-assert len(overlap_genes) == len(genes)
-
-grn = grn.loc[genes, genes]
-
-grn.to_csv("GRN_static.csv")
-
 # %%
-
